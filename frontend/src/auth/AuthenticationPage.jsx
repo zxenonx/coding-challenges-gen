ffbbe732423d8bf825d22562e9a46220800f881e
zxenonx <xenon.nemrod@gmail.com>
2025-06-03 20:38:46 +0100

    Create auth page

---
+++ frontend/src/auth/AuthenticationPage.jsx
@@ -0,0 +1,23 @@
+import "react"
+import {SignIn, SignUp, SignedIn, SignedOut} from '@clerk/clerk-react';
+
+function AuthenticationPage() {
+    return (
+        <div className="auth-container">
+            <SignedIn>
+                <div className="redirect-message">
+                    <h1>Welcome Back!</h1>
+                    <p>You are already signed in.</p>
+                </div>
+            </SignedIn>
+
+            <SignedOut>
+                <h1>Sign In or Sign Up</h1>
+                <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+                <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+            </SignedOut>
+        </div>
+    );
+}
+
+export default HistoryPanel;
