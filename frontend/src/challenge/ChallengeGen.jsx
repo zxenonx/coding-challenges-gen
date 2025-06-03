4389756d516c907b6c29b2cbee92ed0ddaf55642
zxenonx <xenon.nemrod@gmail.com>
2025-06-03 20:39:19 +0100

    Create ChallengeGen.jsx

---
+++ frontend/src/challenge/ChallengeGen.jsx
@@ -0,0 +1,10 @@
+import "react"
+
+export function ChallengeGen() {
+  return (
+    <div>
+      <h1>Challenge Generator</h1>
+      <p>This component will generate challenges based on user input.</p>
+    </div>
+  );
+}
